7a7ad7495b74d2e93b56e75024fe26031f448c59
Daniil Dodin <dankadodin@gmail.com>
2022-10-18 21:40:04 +0300

    fix(lw0): добавление проверки в функцию сумматора времени

--- lw0/time_sum.php
+++ lw0/time_sum.php
@@ -1,8 +1,10 @@
 <?php
 function sumTime(string $oneTime, string $twoTime): string
 {
+    if ((strtotime($oneTime) === false) || (strtotime($twoTime) === false)) {
+        return 'incorrect input';
+    }
     $sumTimeString = date('H:i:s', strtotime($oneTime) + strtotime($twoTime));
     return $sumTimeString;
 }
-
-echo sumTime('15:20:30', '10:20:30');
+echo sumTime('15:15:15', '10:20:30');
