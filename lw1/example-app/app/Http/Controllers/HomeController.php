4aa54ff532de96f1eae18e39f62f3d8c9579aa66
Daniil Dodin <dankadodin@gmail.com>
2022-11-10 21:00:52 +0300

    commit

---
+++ lw1/example-app/app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\HomeControllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+
+class HomeController extends Controller
+{
+public function index() : View
+{
+return view('home', [
+'Blockone' => 'Под куполом ',
+'contentone' =>'тутуутут',
+'Blocktwo' => 'Кэрри',
+'contenttwo' =>'тутутут',
+'Blockthree' => 'Бессонница',
+'contentthree' =>'тутутут',
+'Blockfore' => 'Бессонница',
+'contentfore' =>'тутутут',
+]);
+}
+}
